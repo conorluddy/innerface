6c96a84a0129c118f85fc89b148a714caeed5c8e
Conor Luddy <conorluddy@gmail.com>
2024-11-28 19:37:46 +0000

    Little number slider experiment

--- src/components/NumberSlide/NumberSlide.ts
+++ src/components/NumberSlide/NumberSlide.ts
@@ -1,18 +1,68 @@
-import css from "./NumberSlide.styles";
+import css from "./NumberSlide.css?raw";
 
 const template = document.createElement("template");
 
 template.innerHTML = `
-  <style>${css}</style><slot/>
+  <style>${css}</style>
+  <div class="number-container"></div>
 `;
 
 class NumberSlide extends HTMLElement {
+  private min: number = 0;
+  private max: number = 10;
+
   constructor() {
     super();
-  }
-  connectedCallback() {
     this.attachShadow({ mode: "open" });
     this.shadowRoot?.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    this.updateMinMax();
+    this.renderNumbers();
+  }
+
+  static get observedAttributes() {
+    return ["min", "max"];
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    console.log({ name });
+    if (oldValue !== newValue) {
+      this.updateMinMax();
+      this.renderNumbers();
+    }
+  }
+
+  private updateMinMax() {
+    this.min = parseInt(this.getAttribute("min") || "0", 10);
+    this.max = parseInt(this.getAttribute("max") || "9", 10);
+  }
+
+  private renderNumbers() {
+    const container = this.shadowRoot?.querySelector(".number-container");
+    const innerElement = document.createElement("div");
+    innerElement.classList.add("inner");
+    if (container) {
+      container.innerHTML = "";
+      container.appendChild(innerElement);
+      for (let i = this.min; i <= this.max; i++) {
+        const numberElement = document.createElement("button");
+        numberElement.textContent = i.toString();
+        numberElement.classList.add("number-item");
+        numberElement.addEventListener("click", () => this.onNumberClick(i));
+        innerElement.appendChild(numberElement);
+      }
+    }
+  }
+
+  private onNumberClick(number: number) {
+    const event = new CustomEvent("number-selected", {
+      detail: number,
+      bubbles: true,
+      composed: true,
+    });
+    this.dispatchEvent(event);
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import "./style.css";
-import viteLogo from "/vite.svg";
 import webComponents from "./components";
 
 webComponents();
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <background-noise></background-noise> 
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-  </div>
-`;
+const numberSlide = document.querySelector("number-slide");
+
+numberSlide?.addEventListener("number-selected", (event) => {
+  console.log("Selected number:", event);
+});
