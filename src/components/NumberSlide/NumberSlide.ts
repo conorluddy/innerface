20ccd6ac434672af543fa97e04250fa340c4b6f6
Conor Luddy <conorluddy@gmail.com>
2024-11-30 09:15:27 +0000

    Scroll set not working yet

--- src/components/NumberSlide/NumberSlide.ts
+++ src/components/NumberSlide/NumberSlide.ts
@@ -13,7 +13,9 @@ class NumberSlide extends HTMLElement {
   private min: number = 1;
   private max: number = 10;
   private step: number = 1;
-  private _value: number = 1;
+
+  private index: number = 0;
+  private valuesArray: number[] = [];
 
   constructor() {
     super();
@@ -21,44 +23,60 @@ class NumberSlide extends HTMLElement {
     this.shadowRoot?.appendChild(template.content.cloneNode(true));
   }
 
-  get value(): number {
-    return this._value;
-  }
-
-  set value(newValue: number) {
-    if (newValue >= this.min && newValue <= this.max) {
-      this._value = newValue;
-      this.setAttribute("value", newValue.toString());
-      this.updateScrollPosition();
-      this.dispatchEvent(new Event("change"));
-    }
-  }
-
   connectedCallback() {
     this.updateAttributes();
     this.renderNumbers();
+    window.requestAnimationFrame(() => {
+      this.setupEventListeners();
+    });
   }
 
   static get observedAttributes() {
     return ["min", "max", "value", "label", "step"];
   }
 
+  get value(): number {
+    // We have two things to track here - the index of the selected value
+    // is what lets us position the slider in the correct place, but the
+    // current value will only match the index if we have a min of 0 and
+    // a step of 1 - so we need to interface between the two scales.
+    // Incoming value needs to set the index, but the value needs to be
+    // whatever the index * step value is so that it can be used externally.
+    return this.valuesArray[this.index];
+  }
+
+  set value(newValue: number) {
+    // Invalid:: Return early
+    if (newValue < this.min || newValue > this.max) return;
+    // Convert the index to the actual number seen in the UI
+    this.setAttribute("value", this.valuesArray[this.valueIndex].toString());
+
+    // this.updateScrollPosition();
+  }
+
+  get valueIndex(): number {
+    return this.valuesArray.findIndex((v) => v === this.value);
+  }
+
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
     if (oldValue !== newValue) {
       if (name === "value") {
-        this.value = parseInt(newValue, 10);
+        this.value = parseFloat(newValue);
+        this.updateScrollPosition();
       } else {
         this.updateAttributes();
         this.renderNumbers();
       }
-      this.setupEventListeners();
     }
   }
 
   private updateScrollPosition() {
+    console.log("this.valueIndex", this.valueIndex);
+
     const container = this.shadowRoot?.querySelector(".number-container");
     if (container) {
-      container.scrollLeft = (this._value - this.min) * 80;
+      console.log("this.valueIndex * 80", this.valueIndex * 80);
+      container.scrollLeft = this.valueIndex * 80;
     }
   }
 
@@ -83,11 +101,11 @@ class NumberSlide extends HTMLElement {
       label.innerHTML = this.label;
       container.innerHTML = "";
       container.appendChild(innerElement);
-
       innerElement.appendChild(innerSpacerLeft);
       innerSpacerLeft.classList.add("spacer");
 
       for (let i = this.min; i <= this.max; i += this.step) {
+        this.valuesArray.push(i);
         const numberElement = document.createElement("div");
         numberElement.textContent = i.toString();
         numberElement.classList.add("number-item");
@@ -119,11 +137,14 @@ class NumberSlide extends HTMLElement {
 
     const handleScroll = (event: Event) => {
       lastKnownScrollPosition = (event.target as Element).scrollLeft + 40;
-
       if (!ticking) {
         window.requestAnimationFrame(() => {
           if (lastKnownScrollPosition % 80 === 0) {
-            this.value = lastKnownScrollPosition / 80;
+            const selectedIndex = lastKnownScrollPosition / 80;
+
+            console.log(this.valuesArray[selectedIndex - 1]);
+
+            this.value = this.valuesArray[selectedIndex - 1];
           }
           ticking = false;
         });
